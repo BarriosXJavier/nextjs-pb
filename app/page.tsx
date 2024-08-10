53e75c1f0cd357c0d80a0984fe8197bba11bdf9c
David Muriithi <muriithid05@gmail.com>
2024-08-10 11:26:38 +0300

    nextjs version update to 14.2.5, setup tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,47 @@
+import "./globals.css"
 import { login } from './actions';
-import classes from './page.module.css';
+
 
 export default function Page() {
   return (
-    <main className={classes.main}>
-      Login form
-      <form className={classes.form} action={login}>
-        <label className={classes.label}>
-          E-mail
-          <input name="email" type="email" />
-        </label>
-        <label className={classes.label}>
-          Password
-          <input name="password" type="password" />
-        </label>
-        <button type="submit">Login</button>
-      </form>
+    <main className="flex justify-center items-center h-screen">
+      <div className="w-full max-w-md">
+        <h1 className="text-2xl font-bold mb-4">Login form</h1>
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" action={login}>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+              E-mail
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </main>
   );
 }
